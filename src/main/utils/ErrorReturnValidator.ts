93907a6e599585651863d4ec9333d2cd6905525a
O Daniel <soumenorgame@gmail.com>
2021-12-12 12:19:23 -0300

    chore: remove error return validator

--- src/main/utils/ErrorReturnValidator.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationError } from 'yup';
-import { badRequest } from './ApiResponse';
-import { formatYupError } from './formatters/yupFormatterError';
-
-export async function errorReturnValidator(error: any) {
-  if (error instanceof ValidationError) {
-    return badRequest(formatYupError(error));
-  }
-}
